b98c5b34d69e88f6ff3414713e13c5e441810c0c
bayje8 <k.j.balachandar@gmail.com>
2019-03-17 03:39:41 +0530

    updating add project

--- src/app/addproject/addproject.component.ts
+++ src/app/addproject/addproject.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { TaskserviceService } from '../taskservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { project } from '../project';
 
 @Component({
   selector: 'app-addproject',
@@ -7,9 +10,51 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./addproject.component.css']
 })
 export class AddprojectComponent implements OnInit {
-addProjectForm: FormGroup;
-  constructor() { }
+  addProjectForm: FormGroup;
+  addflag: boolean = true;
+  projects: Array<project> = [];
+  validationMsg: boolean;
+  aProject: project = new project();
+  updatingProject: project = new project();
+  constructor(private taskService: TaskserviceService, private route: ActivatedRoute, private router: Router) {
+    this.initform();
+    this.getProjects();
+  }
+
   ngOnInit() {
   }
 
+  initform() {
+    this.addProjectForm = new FormGroup({
+      iProject: new FormControl(),
+      iStartDate: new FormControl(),
+      iEndDate: new FormControl(),
+      iPriority: new FormControl(),
+      iManager: new FormControl()
+    });
+  }
+
+  getProjects() {
+
+  }
+
+  onSubmit() {
+    if (this.addflag) {
+      this.addProject();
+    } else {
+      this.updateProject();
+    }
+  }
+
+  addProject() {
+this.aProject.project = this.addProjectForm.get("iProject").value;
+this.aProject.startDate = this.addProjectForm.get("iStartDate").value;
+this.aProject.endDate = this.addProjectForm.get("iEndDate").value;
+this.aProject.priority = this.addProjectForm.get("iPriority").value;
+this.aProject.project = this.addProjectForm.get("iManager").value;
+  }
+
+  updateProject() {
+
+  }
 }
---
+++ src/app/project.ts
@@ -0,0 +1,10 @@
+export class project{
+    project_id: number;
+	project:string;
+	startDate:string;
+	endDate:string;
+	priority:string;
+    user_id:number;
+    noOfTasks:number;
+    noOfCompletedTasks:number;
+}
